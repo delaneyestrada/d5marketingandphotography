1a78c2148541b1d806edd226909ee21a637eb6ed
Dillon Estrada <dillonestrada@pop-os.localdomain>
2020-09-10 00:07:57 -0500

    add gallery functionality

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(() => {
+//     document.querySelectorAll('#gallery-nav a').forEach(item => {
+//         console.log(item)
+//         item.addEventListener('click', event => {
+//             event.preventDefault;
+//             console.log(event);
+//         })
+//     })
+// }) 
